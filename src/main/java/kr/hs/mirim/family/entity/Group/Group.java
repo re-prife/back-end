e98faa13a487bdf8ba5ca7d6c3110c45c2c0454a
가연우 <81597104+yeonwoo1125@users.noreply.github.com>
2022-04-26 22:12:45 +0900

    [ADD] 회원, 그룹 엔티티 생성 (#25)
    * [ADD] 유저, 그룹 엔티티 생성
    
    * [UPDATE] User-> User.java, Group -> Group.java 폴더 구조 변경 및 Group OneToMany 설정

---
+++ src/main/java/kr/hs/mirim/family/entity/Group/Group.java
@@ -0,0 +1,35 @@
+package kr.hs.mirim.family.entity.Group;
+
+import kr.hs.mirim.family.entity.User.User;
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "group_tb")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+public class Group {
+    @Id
+    @Column(name = "group_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long groupId;
+
+    @Column(nullable = false, name = "group_invite_code", columnDefinition = "CHAR(7)")
+    private String groupInviteCode;
+
+    @Column(length = 20, nullable = false, name="group_name")
+    private String groupName;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "group")
+    private List<User> userList = new ArrayList<>();
+
+    @Builder
+    public Group(String groupInviteCode, String groupName){
+        this.groupInviteCode = groupInviteCode;
+        this.groupName = groupName;
+    }
+}
---
+++ src/main/java/kr/hs/mirim/family/entity/User/User.java
@@ -0,0 +1,43 @@
+package kr.hs.mirim.family.entity.User;
+
+import kr.hs.mirim.family.entity.Group.Group;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user_tb")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(length = 20, nullable = false, name = "user_name")
+    private String userName;
+
+    @Column(length = 50, nullable = false, name = "user_password")
+    private String userPassword;
+
+    @Column(length = 50, nullable = false, name = "user_email")
+    private String userEmail;
+
+    @Column(length = 10, nullable = false, name = "user_image_name")
+    private String userImageName;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "group_id")
+    private Group group;
+
+    @Builder
+    public User(String userName, String userPassword, String userEmail, String userImageName) {
+        this.userName = userName;
+        this.userEmail = userEmail;
+        this.userPassword = userPassword;
+        this.userImageName = userImageName;
+    }
+}
