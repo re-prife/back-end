6de4e5bef99062cbef5cc5d6d9c046039b3a385a
SRin23 <serin4723@naver.com>
2022-04-27 16:38:08 +0900

    add : Exception 처리 #15
    * Null값이 들어올 경우, 이메일 형식에 맞지 않는 경우에는 FormValidateException으로 처리하였습니다.
    * 이미 가입된 Email로 회원가입 요청이 들어온 경우에는 AlreadyExistsException으로 처리하였습니다.

--- src/main/java/kr/hs/mirim/family/service/UserService.java
+++ src/main/java/kr/hs/mirim/family/service/UserService.java
@@ -3,6 +3,8 @@ package kr.hs.mirim.family.service;
 import kr.hs.mirim.family.dto.request.UserSignUpDto;
 import kr.hs.mirim.family.entity.user.User;
 import kr.hs.mirim.family.entity.user.repository.UserRepository;
+import kr.hs.mirim.family.exception.AlreadyExistsException;
+import kr.hs.mirim.family.exception.FormValidateException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -25,11 +27,11 @@ public class UserService {
     @Transactional
     public void signUp(UserSignUpDto userSignUpDto, BindingResult bindingResult){
         if(bindingResult.hasErrors()){
-            throw new IllegalStateException("유효하지 않은 형식입니다.");
+            throw new FormValidateException("유효하지 않은 형식입니다.");
         }
 
         if(userRepository.existsByUserEmail(userSignUpDto.getUserEmail())){
-            throw new IllegalStateException("이미 가입된 회원입니다.");
+            throw new AlreadyExistsException("이미 가입된 회원입니다.");
         }
 
         String encodePassword = passwordEncoder.encode(userSignUpDto.getUserPassword());
