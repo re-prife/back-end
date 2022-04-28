f5b3ad18c8397249618d51a65d3370a8a02f245c
Min J <69101860+m04j00@users.noreply.github.com>
2022-04-28 15:57:40 +0900

    #23 그룹 가입 기능 (#32)
    * feat : 그룹 가입 기능 구현 #23
    
    * feat : 그룹 가입 기능 구현 #23
    
    * feat : 그룹 가입 기능 구현 #23

--- src/main/java/kr/hs/mirim/family/entity/group/repository/GroupRepository.java
+++ src/main/java/kr/hs/mirim/family/entity/group/repository/GroupRepository.java
@@ -3,6 +3,10 @@ package kr.hs.mirim.family.entity.group.repository;
 import kr.hs.mirim.family.entity.group.Group;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface GroupRepository extends CrudRepository<Group, Long> {
     boolean existsByGroupInviteCode(String code);
+
+    Optional<Group> findByGroupInviteCode(String code);
 }
