988b5091b75702e83874fb43b42e8cd0774814a0
Min J <m04j00@gmail.com>
2022-05-18 20:26:43 +0900

    refactor : request dto에 매개변수 없는 생성자 생성 어노테이션 추가

--- src/main/java/kr/hs/mirim/family/dto/request/CreateGroupRequest.java
+++ src/main/java/kr/hs/mirim/family/dto/request/CreateGroupRequest.java
@@ -2,11 +2,13 @@ package kr.hs.mirim.family.dto.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotEmpty;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class CreateGroupRequest {
     @NotEmpty
     private String groupName;
--- src/main/java/kr/hs/mirim/family/dto/request/JoinGroupRequest.java
+++ src/main/java/kr/hs/mirim/family/dto/request/JoinGroupRequest.java
@@ -2,12 +2,13 @@ package kr.hs.mirim.family.dto.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class JoinGroupRequest {
     @NotEmpty
     private String groupInviteCode;
