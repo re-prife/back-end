b81ba3cb6ec80e2b9c658f3ebb379b25aa03ff1e
Min J <m04j00@gmail.com>
2022-05-19 19:06:41 +0900

    chore : 409 예외처리 클래스 이름 AlreadyExistsException -> Conflict Exception으로 변경 #12

--- src/main/java/kr/hs/mirim/family/exception/ConflictException.java
+++ src/main/java/kr/hs/mirim/family/exception/ConflictException.java
@@ -3,10 +3,10 @@ package kr.hs.mirim.family.exception;
 import kr.hs.mirim.family.exception.handler.ApiException;
 import org.springframework.http.HttpStatus;
 
-public class AlreadyExistsException extends ApiException {
+public class ConflictException extends ApiException {
     private static final int status = HttpStatus.CONFLICT.value();
 
-    public AlreadyExistsException(String message) {
+    public ConflictException(String message) {
         super(status, message);
 
     }
