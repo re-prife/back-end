a4ca4fbdd37fcbfdb1c9779656cbf0bc4ef86386
가연우 <81597104+yeonwoo1125@users.noreply.github.com>
2022-05-21 20:08:51 +0900

    fix : value to enum 메서드 삭제 RequestParam 으로 enum 이름이 들어오므로 필요가 없어짐

--- src/main/java/kr/hs/mirim/family/entity/ingredient/IngredientSaveType.java
+++ src/main/java/kr/hs/mirim/family/entity/ingredient/IngredientSaveType.java
@@ -1,6 +1,5 @@
 package kr.hs.mirim.family.entity.ingredient;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -13,16 +12,4 @@ public enum IngredientSaveType {
 
     private String saveType;
 
-    @JsonCreator
-    public static IngredientSaveType fromValue(String value){
-        switch (value){
-            case "냉장" :
-                return IngredientSaveType.FRIDGE;
-            case "냉동" :
-                return IngredientSaveType.FREEZER;
-            case "실온" :
-                return IngredientSaveType.ROOM_TEMP;
-        }
-        return null;
-    }
 }
