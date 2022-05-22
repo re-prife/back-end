7e2da63e2292a1c2a688a7c821b54a65281e02cb
SRin23 <serin4723@naver.com>
2022-05-22 23:17:29 +0900

    refactor : 이미 존재하는 집안일 확인 후 예외 처리 #6
    * 같은날, 같은 카테고리, 같은 사람이 집안일을  생성할 경우, 같은 집안일로 처리하여 이미 존재하는 집안일이라는 예외 발생

--- src/main/java/kr/hs/mirim/family/entity/chore/repository/ChoreRepository.java
+++ src/main/java/kr/hs/mirim/family/entity/chore/repository/ChoreRepository.java
@@ -1,8 +1,10 @@
 package kr.hs.mirim.family.entity.chore.repository;
 
 import kr.hs.mirim.family.entity.chore.Chore;
+import kr.hs.mirim.family.entity.chore.ChoreCategory;
 import org.springframework.data.repository.CrudRepository;
 
+import java.time.LocalDate;
 public interface ChoreRepository extends CrudRepository<Chore, Long> {
-
+    boolean existsByChoreDateAndChoreCategoryAndUser_UserId(LocalDate date, ChoreCategory choreCategory, Long userId);
 }
