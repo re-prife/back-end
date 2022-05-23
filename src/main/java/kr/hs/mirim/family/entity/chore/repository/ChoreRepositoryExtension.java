dfec4eaca295d4b70ec583ea67cfd5e8931fbba0
Serin Kim <serin4723@naver.com>
2022-05-23 13:56:23 +0900

    refactor : findByChoreGroupAndDate를 findByChoreGroup_GroupIdAndDate로 변경 #38
    * 해당 집안일이 포함된 그룹의 아이디와 날짜를 찾는 메서드 명을 위와 같이 변경하여 가독성을 높이고자 하였습니다.

--- src/main/java/kr/hs/mirim/family/entity/chore/repository/ChoreRepositoryExtension.java
+++ src/main/java/kr/hs/mirim/family/entity/chore/repository/ChoreRepositoryExtension.java
@@ -3,9 +3,8 @@ package kr.hs.mirim.family.entity.chore.repository;
 import kr.hs.mirim.family.dto.response.ChoreListOneDayDataResponse;
 
 import java.time.LocalDate;
-import java.time.YearMonth;
 import java.util.List;
 
 public interface ChoreRepositoryExtension {
-    List<ChoreListOneDayDataResponse> findByChoreGroupAndDate(Long groupId, LocalDate date);
+    List<ChoreListOneDayDataResponse> findByChoreGroup_GroupIdAndDate(Long groupId, LocalDate date);
 }
