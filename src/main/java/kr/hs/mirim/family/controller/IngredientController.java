794630e8b56bf965e8b49e83faa9af8301859c91
가연우 <81597104+yeonwoo1125@users.noreply.github.com>
2022-05-23 22:54:50 +0900

    #47 식재료 전체 조회 기능 구현 (#48)
    * fix : 유통기한 및 구매 날짜 타입 Date -> LocalDate 로 변경
    
    * feat : String으로 들어온 값을 enum으로 변환
    
    * feat : 식재료 추가 기능 구현 #3
    그룹이 존재하는 체크
    json으로 들어온 값이 제대로 들어왔는지 체크
    
    * feat : 식재료 전체 조회 기능 구현
    전체 조회 및 저장소별 조회
    
    * fix : value to enum 메서드 삭제
    RequestParam 으로 enum 이름이 들어오므로 필요가 없어짐
    
    * fix : defaultValue 속성 삭제
    식재료 리스트 반환 메서드에서 삭제함
    
    * fix : 식재료 이름, 이미지 파일 NotEmpty 처리
    ingredientName, ingredientImageName
    
    * fix : saveType null 처리, QIngredient static 처리
    saveType이 null일 경우 where 조건문에서 제외
    
    * fix : 에러 형식 수정

--- src/main/java/kr/hs/mirim/family/controller/IngredientController.java
+++ src/main/java/kr/hs/mirim/family/controller/IngredientController.java
@@ -1,12 +1,14 @@
 package kr.hs.mirim.family.controller;
 
 import kr.hs.mirim.family.dto.request.CreateIngredientRequest;
+import kr.hs.mirim.family.dto.response.IngredientListResponse;
 import kr.hs.mirim.family.service.IngredientService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,5 +19,10 @@ public class IngredientController {
     @PostMapping
     public void createIngredient(@PathVariable long groupId, @Valid @RequestBody CreateIngredientRequest request, BindingResult bindingResult){
         service.createIngredient(request, groupId, bindingResult);
+    }
+
+    @GetMapping
+    public List<IngredientListResponse> ingredientList(@PathVariable long groupId, @RequestParam(required = false) String saveType){
+        return service.ingredientSaveTypeList(groupId, saveType);
     }
 }
--- src/main/java/kr/hs/mirim/family/entity/ingredient/repository/IngredientRepository.java
+++ src/main/java/kr/hs/mirim/family/entity/ingredient/repository/IngredientRepository.java
@@ -3,5 +3,5 @@ package kr.hs.mirim.family.entity.ingredient.repository;
 import kr.hs.mirim.family.entity.ingredient.Ingredient;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface IngredientRepository extends JpaRepository<Ingredient, Long> {
+public interface IngredientRepository extends JpaRepository<Ingredient, Long>, IngredientRepositoryExtension {
 }
