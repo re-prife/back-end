efe88ac8e54139ab5144e86f4e746c1331f3a5c7
m04j00 <m04j00@gmail.com>
2022-05-27 14:55:59 +0900

    fix : 공지사항이 빈 값으로 들어올 경우 예외처리 #70

--- src/main/java/kr/hs/mirim/family/dto/request/ReportRequest.java
+++ src/main/java/kr/hs/mirim/family/dto/request/ReportRequest.java
@@ -5,13 +5,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotEmpty;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class ReportRequest {
-    @NotNull
+    @NotEmpty
     @Length(max=50)
     private String groupReport;
 }
