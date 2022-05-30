c1bbde85350c9f60e43a20554bfff00bd22d974c
m04j00 <m04j00@gmail.com>
2022-05-30 15:14:14 +0900

    add : IngredientController 관련 Swagger 업데이트 #89

--- src/main/java/kr/hs/mirim/family/controller/IngredientController.java
+++ src/main/java/kr/hs/mirim/family/controller/IngredientController.java
@@ -1,52 +1,98 @@
 package kr.hs.mirim.family.controller;
 
+import io.swagger.annotations.ApiParam;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import kr.hs.mirim.family.dto.request.DeleteIngredientRequest;
 import kr.hs.mirim.family.dto.request.IngredientRequest;
 import kr.hs.mirim.family.dto.request.UpdateIngredientCountRequest;
 import kr.hs.mirim.family.dto.response.IngredientListResponse;
 import kr.hs.mirim.family.service.IngredientService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
 
+@Tag(name = "INGREDIENT", description = "식재료 API")
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("groups/{groupId}/ingredients")
+@RequestMapping("/groups/{groupId}/ingredients")
 public class IngredientController {
     private final IngredientService ingredientService;
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "식재료 생성 성공"),
+            @ApiResponse(responseCode = "400", description = "Request 형식이 유효하지 않을 경우"),
+            @ApiResponse(responseCode = "404", description = "groupId가 존재하지 않을 경우"),
+            @ApiResponse(responseCode = "409", description = "식재료의 수량이 0인 경우")
+    })
+    @Operation(tags = "INGREDIENT", summary = "식재료 생성", description = "식재료를 생성하는 API")
     @PostMapping
-    public void createIngredient(@PathVariable long groupId, @Valid @RequestBody IngredientRequest request, BindingResult bindingResult){
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public void createIngredient(@ApiParam(value = "식재료를 생성할 그룹의 ID") @PathVariable long groupId,
+                                 @Valid @RequestBody IngredientRequest request,
+                                 BindingResult bindingResult
+    ) {
         ingredientService.createIngredient(request, groupId, bindingResult);
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "식재료 조회 성공"),
+            @ApiResponse(responseCode = "500", description = "saveType이 유효하지 않을 경우"),
+            @ApiResponse(responseCode = "404", description = "groupId가 존재하지 않을 경우")
+    })
+    @Operation(tags = "INGREDIENT", summary = "식재료 조회", description = "식재료를 전체 조회하거나 저장 방법 별로 조회하는 API")
     @GetMapping
-    public List<IngredientListResponse> ingredientList(@PathVariable long groupId, @RequestParam(required = false) String saveType){
+    public List<IngredientListResponse> ingredientList(@ApiParam(value = "식재료를 조회할 그룹의 ID") @PathVariable long groupId,
+                                                       @ApiParam(value = "식재료를 조회할 저장 방법") @RequestParam(required = false) String saveType) {
         return ingredientService.ingredientSaveTypeList(groupId, saveType);
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "식재료 갱신 성공"),
+            @ApiResponse(responseCode = "400", description = "Request 형식이 유효하지 않을 경우"),
+            @ApiResponse(responseCode = "404", description = "groupId 또는 ingredientId가 존재하지 않을 경우\n\n식재료가 그룹에 속하지 않을 경우"),
+    })
+    @Operation(tags = "INGREDIENT", summary = "식재료 갱신", description = "식재료 상세 정보를 갱신하는 API")
     @PutMapping("/{ingredientId}")
-    public void updateIngredient(@PathVariable long groupId,
-                                 @PathVariable long ingredientId,
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    public void updateIngredient(@ApiParam(value = "식재료가 속한 그룹의 ID") @PathVariable long groupId,
+                                 @ApiParam(value = "갱신할 식재료의 ID") @PathVariable long ingredientId,
                                  @RequestBody @Valid IngredientRequest request,
-                                 BindingResult result){
+                                 BindingResult result) {
         ingredientService.updateIngredient(groupId, ingredientId, request, result);
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "식재료 삭제 성공"),
+            @ApiResponse(responseCode = "400", description = "Request 형식이 유효하지 않을 경우"),
+            @ApiResponse(responseCode = "404", description = "groupId 또는 식재료가 존재하지 않을 경우\n\n식재료가 그룹에 속하지 않을 경우"),
+    })
+    @Operation(tags = "INGREDIENT", summary = "식재료 삭제", description = "식재료를 삭제하는 API")
     @DeleteMapping
-    public void deleteIngredient(@PathVariable long groupId,
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    public void deleteIngredient(@ApiParam(value = "식재료가 속한 그룹의 ID") @PathVariable long groupId,
                                  @RequestBody @Valid DeleteIngredientRequest request,
-                                 BindingResult result){
+                                 BindingResult result) {
         ingredientService.deleteIngredient(groupId, request, result);
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "식재료 수량 갱신 성공"),
+            @ApiResponse(responseCode = "400", description = "Request 형식이 유효하지 않을 경우"),
+            @ApiResponse(responseCode = "404", description = "groupId 또는 식재료가 존재하지 않을 경우\n\n식재료가 그룹에 속하지 않을 경우"),
+    })
+    @Operation(tags = "INGREDIENT", summary = "식재료 일괄 수량 갱신", description = "식재료의 수량을 일괄로 갱신하는 API")
     @PutMapping
-    public void updateIngredientCount(@PathVariable long groupId,
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    public void updateIngredientCount(@ApiParam(value = "식재료가 속한 그룹의 ID") @PathVariable long groupId,
                                       @RequestBody @Valid UpdateIngredientCountRequest request,
-                                      BindingResult result){
+                                      BindingResult result) {
         ingredientService.updateIngredientCount(groupId, request, result);
     }
 
--- src/main/java/kr/hs/mirim/family/dto/response/IngredientListResponse.java
+++ src/main/java/kr/hs/mirim/family/dto/response/IngredientListResponse.java
@@ -1,5 +1,6 @@
 package kr.hs.mirim.family.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import kr.hs.mirim.family.entity.ingredient.IngredientCategory;
 import kr.hs.mirim.family.entity.ingredient.IngredientSaveType;
 import lombok.AllArgsConstructor;
@@ -10,26 +11,33 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
+@Schema(description = "식재료 조회 Response")
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class IngredientListResponse {
 
+    @Schema(description = "식재료 이름", example = "햇감자")
     @NotEmpty
     private String ingredientName;
 
+    @Schema(description = "식재료 저장 방법", example = "FRIDGE")
     @NotNull
     private IngredientSaveType ingredientSaveType;
 
+    @Schema(description = "식재료 유통기한", example = "2023-03-01")
     @NotNull
     private LocalDate ingredientExpirationDate;
 
+    @Schema(description = "식재료 카테고리", example = "VEGGIE")
     @NotNull
     private IngredientCategory ingredientCategory;
 
+    @Schema(description = "식재료 수량", example = "3개")
     @NotEmpty
     private String ingredientCount;
 
+    @Schema(description = "식재료 이미지 주소", example = "6ce716eeb4")
     @NotEmpty
     private String ingredientImageName;
 }
