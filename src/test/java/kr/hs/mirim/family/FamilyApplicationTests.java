aba7c14c03b752388d082c38c87c644e6f62a866
가연우 <81597104+yeonwoo1125@users.noreply.github.com>
2022-06-06 22:54:53 +0900

    test : @SpringBootTest 어노테이션 삭제 gradlew 빌드 시 오류 발생으로 인한 삭제

--- src/test/java/kr/hs/mirim/family/FamilyApplicationTests.java
+++ src/test/java/kr/hs/mirim/family/FamilyApplicationTests.java
@@ -3,7 +3,7 @@ package kr.hs.mirim.family;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class FamilyApplicationTests {
 
     @Test
