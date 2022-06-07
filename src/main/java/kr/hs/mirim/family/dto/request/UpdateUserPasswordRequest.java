d45dbb8111c3162b089d86726de5c4070453e254
SRin23 <serin4723@naver.com>
2022-06-07 17:11:53 +0900

    fix : 비밀번호의 최소길이 확인 #112
    * 비밀번호의 최소길이가 8자리인지 확인하는 코드입니다.
    * 비밀번호의 최소길이가 8자리 이하인 경우 BindingResult에서 형식이 옳지 않다는 예외를 반환합니다.

--- src/main/java/kr/hs/mirim/family/dto/request/UpdateUserPasswordRequest.java
+++ src/main/java/kr/hs/mirim/family/dto/request/UpdateUserPasswordRequest.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Schema(description = "회원 갱신 Request")
 @Getter
@@ -19,6 +20,7 @@ public class UpdateUserPasswordRequest {
 
     @Schema(description = "변경할 비밀번호", example = "doremifasol")
     @NotEmpty
+    @Size(min=8)
     private String userNewPassword;
 
     @Schema(description = "변경할 비밀번호 확인", example = "doremifasol")
