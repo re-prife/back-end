03578561f137497d11d158366cf062dad9b0141d
s2021 <s2021@e-mirim.hs.kr>
2022-06-08 11:23:39 +0900

    add : 더미데이터 추가 #109

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,10 +1,12 @@
 INSERT INTO GROUP_TB
 VALUES (1, 'mon0516', '민지네', '취준생 민지와 직딩 친구들과 함께 진행하는 프로젝트..^_^');
+INSERT INTO GROUP_TB
+VALUES (2, 'mon0512 ', '민지네2', '취준생 민지와 직딩 친구들과 함께 진행하는 프로젝트.22.^_^');
 
 INSERT INTO USER_TB
 VALUES (1, 'm04j00@gmail.com', '1.png', 'Min J', '취준생', 'doremisolsol', 1);
 INSERT INTO USER_TB
-VALUES (2, 'akwls@gmail.com', '2.png', '마진', '테크타카 1', 'doremisolsol', 1);
+VALUES (2, 'akwls@gmail.com', '2.png', '마진', '테크타카 1', 'doremisolsol', 2);
 INSERT INTO USER_TB
 VALUES (3, 'srin@gmail.com', '3.png', '세라', '테크타카 2', 'doremisolsol', 1);
 INSERT INTO USER_TB
@@ -16,11 +18,21 @@ INSERT INTO INGREDIENT_TB
 VALUES (1, NOW(), NOW(), 'VEGGIE', '3', '2024-02-02', '1.PNG', '햇감자 구워먹기', '햇감자', NOW(), 'ROOM_TEMP', 1);
 INSERT INTO INGREDIENT_TB
 VALUES (2, NOW(), NOW(), 'VEGGIE', '5', '2024-07-02', '2.PNG', '고구마 구워먹기', '왕고구마', NOW(), 'FRIDGE', 1);
+INSERT INTO INGREDIENT_TB
+VALUES (3, NOW(), NOW(), 'VEGGIE', '5', '2024-07-02', '2.PNG', '고구마 구워먹기', '왕고구마', NOW(), 'FRIDGE', 2);
 
 INSERT INTO QUEST_TB
 VALUES (1, NOW(), NOW(), 3, TRUE, '오늘 저녁은 치킨이었으면...', '치킨 사서 올 사람', 1, 1);
 INSERT INTO QUEST_TB
 VALUES (2, NOW(), NOW(), -1, FALSE, '집에 콜라가 없어...', '콜라 사서 올 사람', 1, 1);
+INSERT INTO QUEST_TB
+VALUES (3, NOW(), NOW(), 1, TRUE, '오늘 저녁은 치킨이었으면...', '치킨 사서 올 사람', 1, 3);
+INSERT INTO QUEST_TB
+VALUES (4, NOW(), NOW(), -1, FALSE, '집에 콜라가 없어...', '콜라 사서 올 사람', 1, 4);
+INSERT INTO QUEST_TB
+VALUES (5, NOW(), NOW(), 1, TRUE, '오늘 저녁은 치킨이었으면...', '치킨 사서 올 사람', 1, 5);
+INSERT INTO QUEST_TB
+VALUES (6, NOW(), NOW(), 1, TRUE, '집에 콜라가 없어...', '콜라 사서 올 사람', 1, 2);
 
 INSERT INTO CHORE_TB
 VALUES (1, NOW(), NOW(), 'COOK', 'BEFORE', NOW(), '하늘이 요리하는 날', 1, 4);
