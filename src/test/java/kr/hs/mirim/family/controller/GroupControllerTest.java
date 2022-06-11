c68c731774e34d6141f307ca89ad988792de8a7f
Min J <m04j00@gmail.com>
2022-06-12 02:40:32 +0900

    test : 그룹의 공지사항을 조회 / 갱신하는 테스트 코드 작성 #102

--- src/test/java/kr/hs/mirim/family/controller/GroupControllerTest.java
+++ src/test/java/kr/hs/mirim/family/controller/GroupControllerTest.java
@@ -3,6 +3,7 @@ package kr.hs.mirim.family.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import kr.hs.mirim.family.dto.request.CreateGroupRequest;
 import kr.hs.mirim.family.dto.request.JoinGroupRequest;
+import kr.hs.mirim.family.dto.request.ReportRequest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,8 +14,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -140,6 +140,49 @@ class GroupControllerTest {
     @Test
     void get_users_in_group_404_by_user() throws Exception {
         mockMvc.perform(get("/groups/2/100"))
+                .andExpect(status().isNotFound());
+    }
+
+    @Transactional
+    @Test
+    void 그룹_공지사항_갱신_204() throws Exception {
+        ReportRequest request = new ReportRequest("그룹 공지사항 갱신");
+        mockMvc.perform(put("/groups/1/report")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(request)))
+                .andExpect(status().isNoContent());
+    }
+
+    @Transactional
+    @Test
+    void 그룹_공지사항_갱신_400() throws Exception {
+        ReportRequest request = new ReportRequest("");
+        mockMvc.perform(put("/groups/1/report")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(request)))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Transactional
+    @Test
+    void 그룹_공지사항_갱신_404() throws Exception {
+        ReportRequest request = new ReportRequest("그룹 공지사항 갱신");
+        mockMvc.perform(put("/groups/1/report")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(request)))
+                .andExpect(status().isNoContent());
+    }
+
+    @Test
+    void 그룹_공지사항_조회_200() throws Exception {
+        mockMvc.perform(get("/groups/1/report"))
+                .andExpect(status().isOk());
+    }
+
+    // - groupId가 존재하지 않을 경우
+    @Test
+    void 그룹_공지사항_조회_404() throws Exception {
+        mockMvc.perform(get("/groups/100/report"))
                 .andExpect(status().isNotFound());
     }
 
