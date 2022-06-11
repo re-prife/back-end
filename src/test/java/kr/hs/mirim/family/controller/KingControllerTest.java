9c7855cf94b946e338a3f51b6ec83381e3876d0b
가연우 <81597104+yeonwoo1125@users.noreply.github.com>
2022-06-11 13:59:50 +0900

    fix : @WebAppConfiguration 추가 CodeCov test Failed로 인해 추가

--- src/test/java/kr/hs/mirim/family/controller/KingControllerTest.java
+++ src/test/java/kr/hs/mirim/family/controller/KingControllerTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,6 +19,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @Transactional
+@WebAppConfiguration
 class KingControllerTest {
     MockMvc mvc;
 
