58537ef9ca56cbf1f7c3ead2727c87d3feacbf4f
SRin23 <serin4723@naver.com>
2022-06-12 19:49:00 +0900

    fix : 500 Exception 추가 및 코드 수정 #147
    * 500 InternalServerException 추가
    * e.printStackTrace를 예외처리한 후, 해당 예외 메세지를 반환

--- src/main/java/kr/hs/mirim/family/controller/NotificationController.java
+++ src/main/java/kr/hs/mirim/family/controller/NotificationController.java
@@ -2,6 +2,7 @@ package kr.hs.mirim.family.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import kr.hs.mirim.family.exception.InternalServerException;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
@@ -25,7 +26,7 @@ public class NotificationController {
         try {
             sseEmitter.send(SseEmitter.event().name("connect"));
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new InternalServerException(e.getMessage());
         }
 
         sseEmitters.put(groupId, sseEmitter);
