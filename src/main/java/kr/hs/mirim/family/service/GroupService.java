72b92e2bbfe57af872034b4fae575900a27613c4
m04j00 <m04j00@gmail.com>
2022-06-14 16:09:44 +0900

    fix : 그룹 초대코드 생성 코드를 커버리지 통과할 수 있도록 변경 #102

--- src/main/java/kr/hs/mirim/family/service/GroupService.java
+++ src/main/java/kr/hs/mirim/family/service/GroupService.java
@@ -140,19 +140,23 @@ public class GroupService {
     }
 
     private String createInviteCode() {
-        int leftLimit = 48;
-        int rightLimit = 122;
-        int targetStringLength = 7;
+        char[] chs = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+                'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+                'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'k', 'l', 'm',
+                'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+        };
         Random random = new Random();
-        String code;
+        String randomCode;
+
         do {
-            code = random
-                    .ints(leftLimit, rightLimit + 1)
-                    .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
-                    .limit(targetStringLength)
-                    .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
-                    .toString();
-        } while (groupRepository.existsByGroupInviteCode(code));
-        return code;
+            StringBuilder code = new StringBuilder();
+            for (int i = 0; i < 7; i++) {
+                char ch = chs[random.nextInt(chs.length)];
+                code.append(ch);
+            }
+            randomCode = code.toString();
+        } while (groupRepository.existsByGroupInviteCode(randomCode));
+        return randomCode;
     }
 }
